0caa3ca3f20c0ad14db2bf663c39770b26cea9ab
771073216 <37037387+771073216@users.noreply.github.com>
2024-10-15 20:51:31 +0800

    Add files via upload

---
+++ enable_gpedit.bat
@@ -0,0 +1,27 @@
+@echo off
+
+setlocal enabledelayedexpansion
+set "count=0"
+
+for /f %%i in ('dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum') do (
+set list[!count!]=%%i
+set /a "count+=1"
+echo !count!
+)
+
+for /f %%i in ('dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum') do (
+set list[!count!]=%%i
+set /a "count+=1"
+echo !count!
+)
+
+set count=0
+:SymLoop
+
+if defined list[%count%] ( 
+   call dism /online /norestart /add-package:"C:\Windows\servicing\Packages\%%list[%count%]%%"
+   set /a "count+=1"
+   GOTO :SymLoop 
+)
+
+pause
